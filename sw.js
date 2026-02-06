9de6b2369b98050033bc07d37fddd878bc4bc0b9
EL_PARCERITO <djzebo1019@gmail.com>
2026-02-06 20:51:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,9 +6,13 @@ self.addEventListener("activate", e => {
   self.clients.claim();
 });
 
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
+});
+
 self.addEventListener("notificationclick", e => {
   e.notification.close();
   e.waitUntil(
-    clients.openWindow("/sabores-admin/admin.html")
+    clients.openWindow("/dagusto/admin.html")
   );
 });
